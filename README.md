80078711c15fce931f13d9a30d2930cd60c23942
Ilian Lypota <ilianlypota@gmail.com>
2023-03-09 14:04:00 +0300

    CI/CD prereview

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # yamdb_final
 yamdb_final
+![workflow](https://github.com/IlianL/yamdb_final/actions/workflows/yamdb_workflow.yml/badge.svg)
---
+++ api_yamdb/api/filters.py
@@ -0,0 +1,12 @@
+from django_filters import FilterSet, filters
+from reviews.models import Title
+
+
+class TitleFilter(FilterSet):
+    name = filters.CharFilter(field_name='name', lookup_expr='contains')
+    genre = filters.CharFilter(field_name='genre__slug')
+    category = filters.CharFilter(field_name='category__slug')
+
+    class Meta:
+        model = Title
+        fields = ('name', 'category', 'genre', 'year')
